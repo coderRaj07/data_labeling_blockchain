7c43194b69f9f1344ff7123e06c1e88a9b40e18b
coderRaj07 <12131501.edu@gmail.com>
2024-07-26 22:57:29 +0530

    added express.json for body as input

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import userRouter from './routers/user';
 import workerRouter from './routers/worker';
 const app = express();
+
+app.use(express.json())
 export const JWT_SECRET = "Raj1234"
 app.use("/v1/user", userRouter)
 app.use("/v1/worker", workerRouter)
